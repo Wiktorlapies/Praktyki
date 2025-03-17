8237aad69beb510c26d2ec942a2f4febaaa47d39
Wiktorlapies <wiktorlapies8@gmail.com>
2025-03-18 00:36:20 +0100

    Update CiagFibonacciego.py

--- CiagFibonacciego.py
+++ CiagFibonacciego.py
@@ -4,7 +4,7 @@ wyrazy_cf = {}
 wyrazy_cf[0] = 0
 wyrazy_cf[1] = 1
 
-for i in range(2, n+1):
+for i in range(2, n):
     wyrazy_cf[i] = (wyrazy_cf[i - 1] + wyrazy_cf[i - 2])
 
-print(f"Wyrazem nr {n} ciagu Fibonacciego jest liczba {wyrazy_cf[n]}")
+print(f"Wyrazem nr {n} ciagu Fibonacciego jest liczba {wyrazy_cf[n-1]}")
