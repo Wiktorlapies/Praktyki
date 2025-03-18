30042c6ef758e4b89c11dc81f09a54e339e11b4f
Wiktor <wiktorlapies8@gmail.com>
2025-03-18 13:52:05 +0100

    18.03.25

--- CiagFibonacciego1.py
+++ CiagFibonacciego1.py
@@ -1,12 +1,18 @@
-n = int(input("Podaj ktory wyraz ciagu chcesz zobaczyc(nieujemna liczba calkowita):"))
 
-wyrazy_cf = {}
-wyrazy_cf[0] = 0
-wyrazy_cf[1] = 1
 
-for i in range(2, n+1):
-    wyrazy_cf[i] = (wyrazy_cf[i - 1] + wyrazy_cf[i - 2])
+try:
+   
+    n = int(input("Podaj ktory wyraz ciagu chcesz zobaczyc:"))
+    if n < 0:
+        print("Liczba musi byc nieujemna")
+    else:
+        wyrazy_cf = [0, 1]
+       
+        for i in range(2, n + 1):
+            wyrazy_cf.append((wyrazy_cf[i - 1] + wyrazy_cf[i - 2]))
 
-print(f"Wyrazem nr {n} ciagu Fibonacciego jest liczba {wyrazy_cf[n]}")
+        print(f"Wyrazem nr {n} ciagu Fibonacciego jest liczba {wyrazy_cf[n]}")
 
+except ValueError:
+    print("Podaj liczbe calkowita")
 
